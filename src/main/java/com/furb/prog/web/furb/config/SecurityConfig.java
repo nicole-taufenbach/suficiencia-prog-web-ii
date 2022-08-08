9374c06e49e68c6cee0a20c4d87cd82c2188cd65
Nicole Taufenbach <ntaufenbach@hotmail.com>
2022-08-08 12:21:17 -0300

    ajustes na formatacao

--- src/main/java/com/furb/prog/web/furb/config/SecurityConfig.java
+++ src/main/java/com/furb/prog/web/furb/config/SecurityConfig.java
@@ -38,9 +38,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/v2/api-docs",
                 "/configuration/ui",
+                "/swagger-resources/**",
                 "/configuration/security",
-                "/swagger-ui.html"
-        );
+                "/swagger-ui.html",
+                "/webjars/**");
     }
 
 }
--- src/main/java/com/furb/prog/web/furb/config/SwaggerConfig.java
+++ src/main/java/com/furb/prog/web/furb/config/SwaggerConfig.java
@@ -2,17 +2,18 @@ package com.furb.prog.web.furb.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
 
-import static springfox.documentation.builders.PathSelectors.regex;
-
 @Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
@@ -20,7 +21,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.furb.prog.web.furb"))
-                .paths(regex("/RestAPIFurb/comandas.*"))
+                .paths(PathSelectors.any())
                 .build()
                 .apiInfo(metaInfo());
     }
